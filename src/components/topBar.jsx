78c6156fde91837f8d05032e8e91540f4427388a
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-09-07 09:21:00 +0200

    v2.0

--- src/components/topBar.jsx
+++ src/components/topBar.jsx
@@ -8,44 +8,42 @@ import Scifi from "../data/books/scifi.json";
 class TopBar extends Component {
   render() {
     return (
-      <Navbar expand="lg" style={{ backgroundColor: "gray" }}>
-        <Container fluid="sm">
-          <Navbar.Brand href="#home" className="text-white fs-3">
-            Library-ReactBoostrap
-          </Navbar.Brand>
+      <Navbar expand="lg" style={{ backgroundColor: "gray" }} className="px-3 mb-3">
+        <Navbar.Brand href="#home" className="text-white fs-3">
+          Library-ReactBoostrap
+        </Navbar.Brand>
 
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home" className="text-white">
-                Home
-              </Nav.Link>
-              <Nav.Link href="#link" className="text-white">
-                About
-              </Nav.Link>
-              <Nav.Link href="#browse" className="text-white">
-                Browse
-              </Nav.Link>
-              <NavDropdown title="Categories" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1" onClick={() => this.props.Fn(Fantasy)}>
-                  Fantasy
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2" onClick={() => this.props.Fn(History)}>
-                  History
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3" onClick={() => this.props.Fn(Horror)}>
-                  Horror
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3" onClick={() => this.props.Fn(Romance)}>
-                  Romance
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3" onClick={() => this.props.Fn(Scifi)}>
-                  Scifi
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-secondary" />
-        </Container>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className="text-white">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#link" className="text-white">
+              About
+            </Nav.Link>
+            <Nav.Link href="#browse" className="text-white">
+              Browse
+            </Nav.Link>
+            <NavDropdown title="Categories" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1" onClick={() => this.props.Fn(Fantasy)}>
+                Fantasy
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2" onClick={() => this.props.Fn(History)}>
+                History
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3" onClick={() => this.props.Fn(Horror)}>
+                Horror
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3" onClick={() => this.props.Fn(Romance)}>
+                Romance
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3" onClick={() => this.props.Fn(Scifi)}>
+                Scifi
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-secondary" />
       </Navbar>
     );
   }
